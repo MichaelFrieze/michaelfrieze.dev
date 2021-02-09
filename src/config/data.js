82d7b63e03a418cd5541aaa6bb267e7e476f8187
Michael Frieze <michaelfrieze@icloud.com>
2021-02-08 19:00:08 -0500

    modified the aboutData

--- src/config/data.js
+++ src/config/data.js
@@ -19,10 +19,10 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne:
-    "My name is Michael and I am based in Clinton Township, MI. I found my passion for learning computer sciences at the age of 23 and went to college at age 25. I've dedicated countless hours to learning and expanding this passion. I have developed an avid work ethic and disciplinary skills along my journey in life. Strong creative and critical thinking skills have helped me to excel in areas like web development as well as functional and object-oriented programming.",
+    "My name is Michael and I am based in Clinton Township, MI. I found my passion for programming about 11 years ago and went to college soon after. I've dedicated countless hours to learning and expanding this passion while developing an avid work ethic and disciplinary skills along the way. Strong creative and critical thinking skills have helped me to excel in areas like web development as well as functional and object-oriented programming.",
 
   paragraphTwo:
-    'In my professional career, I started out as a Java developer working at Oakland University. During my time there, the team decided to implement ReactJS and this sparked my interest in JavaScript. I spent the next few years working on various JavaScript and CSS projects while continuing to work as a back-end developer. Now, I am shifting my focus more towards JavaScript and frontend development in general.',
+    'In my professional career, I started out as a Java developer working at Oakland University. During my time there, the team decided to implement ReactJS and this sparked my interest in JavaScript. I spent the next few years working on various JavaScript and CSS projects while continuing to work as a back-end developer. Now, I am shifting my focus towards JavaScript and frontend development in general.',
 
   paragraphThree:
     'When I am not coding, I enjoy playing guitar, watching "The Great Courses", playing PC games, reading, and spending time with my partner (since 2010).',

b0cd807de13f9b9b4c27df70d8e1cdd8bac0063d
Michael Frieze <michaelfrieze@icloud.com>
2021-12-19 17:07:24 -0500

    updatead projects page

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,8 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Here are some projects I have worked on to showcase my skills and my progress as I
+            continue to learn.
           </p>
         </div>
         <div className="container py-12">

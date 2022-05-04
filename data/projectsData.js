912670f427ced0e3968ec870f779416ad5d21dc5
Michael Frieze <michaelfrieze@icloud.com>
2022-05-03 23:18:34 -0400

    added remix kudos project to projectsData and also updated project description for all projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,38 +1,45 @@
 const projectsData = [
   {
     title: 'DJ Events Remix',
-    description: ``,
+    description: `This is the Remix version of Brad Traversy's "DJ Events" Next.js app. You can find a link to the original app in the GitHub repository.`,
     imgSrc: '/static/images/dj-events.jpg',
     href: 'https://github.com/MichaelFrieze/djevents-remix',
     liveLink: 'https://djevents-remix.up.railway.app/',
   },
   {
     title: 'Remix Chat',
-    description: ``,
+    description: `This app uses Remix and Supabase to build a realtime chat application.`,
     imgSrc: '/static/images/remix-chat.jpg',
     href: 'https://github.com/MichaelFrieze/remix-chat',
     liveLink: 'https://remix-chat-michaelfrieze.vercel.app/',
   },
   {
     title: 'Proshop',
-    description: ``,
+    description: `This is the app from Brad Traversy's MERN stack course. It is deployed using Heroku free tier so it takes some time to initially load.`,
     imgSrc: '/static/images/proshop.jpg',
     href: 'https://github.com/MichaelFrieze/proshop',
     liveLink: 'https://michaelfrieze-proshop.herokuapp.com/',
   },
   {
     title: "Slick's Slices",
-    description: ``,
+    description: `This is the app from Wes Bos's "Master Gatsby" course. It is built with Gatsby and uses Sanity for the headless CMS`,
     imgSrc: '/static/images/slicks-slices.jpg',
     href: 'https://github.com/MichaelFrieze/slicks-slices',
     liveLink: 'https://slicksslices-michaelfrieze.netlify.app/',
   },
   {
     title: 'Omnifood',
-    description: ``,
+    description: `A simple app that looks quite nice to showcase CSS skills.`,
     imgSrc: '/static/images/omnifood.jpg',
     href: 'https://github.com/MichaelFrieze/omnifood',
     liveLink: 'https://omnifood-michaelfrieze.netlify.app/',
+  },
+  {
+    title: 'Remix Kudos',
+    description: `An app built with Remix, Prisma, and MongoDB that allows users to send Kudos to each other. It also uses an AWS S3 bucket for image upload and is deployed on Railway.`,
+    imgSrc: '/static/images/remix-kudos.jpg',
+    href: 'https://github.com/MichaelFrieze/remix-kudos',
+    liveLink: 'https://remix-kudos.up.railway.app/',
   },
 ]
 
